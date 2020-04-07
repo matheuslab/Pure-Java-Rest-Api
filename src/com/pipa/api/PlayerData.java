755eefde43bfb68f4ff0c363d2f24f1c424fbb70
Matheus Martins <matheus.martins@ee.ufcg.edu.br>
2020-04-07 01:11:10 -0300

    Creating basic post method

---
+++ src/com/pipa/api/PlayerData.java
@@ -0,0 +1,26 @@
+package com.pipa.api;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import lombok.Builder;
+import lombok.Data;
+
+/*
+  Jackson Builder method is different from lombok builder method, so we need
+  to create a special builder on this class in order to fix this.
+*/
+@Data
+@JsonDeserialize(builder = PlayerData.PlayerDataBuilder.class)
+@Builder(builderClassName = "PlayerDataBuilder", toBuilder = true)
+public class PlayerData {
+
+  private final int userId;
+
+  private final int points;
+
+  @JsonPOJOBuilder(withPrefix = "")
+  public static class PlayerDataBuilder {
+
+  }
+
+}
