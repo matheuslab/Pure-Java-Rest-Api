e6f38d024a307b587400fc8df10c0bf668eed8ec
Matheus Martins <matheus.martins@ee.ufcg.edu.br>
2020-04-08 02:05:20 -0300

    Refactoring Application class

--- src/com/pipa/api/Application.java
+++ src/com/pipa/api/Application.java
@@ -1,107 +1,26 @@
 package com.pipa.api;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pipa.api.handlers.FetchUserPositionHandler;
+import com.pipa.api.handlers.HighScoreHandler;
+import com.pipa.api.handlers.ScoreRegisterHandler;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.stream.Collectors;
 
 public class Application {
-
-    private static ObjectMapper objectMapper = new ObjectMapper();
-    private static Map<Long, Long> playersScore = new HashMap<Long, Long>();
-    private static PlayerData playerData = new PlayerData();
-    private static List<ScorePosition> scorePosition = new ArrayList<ScorePosition>();
 
     public static void main(String[] args) throws IOException {
         int serverPort = 8000;
         HttpServer server = HttpServer.create(new InetSocketAddress(serverPort), 0);
 
-        //GET Score of a user
-        server.createContext("/", (exchange -> {
-            byte[] response;
-            if ("GET".equals(exchange.getRequestMethod())) {
-                String params = exchange.getRequestURI().getPath();
-                String respText = String.format("Hello %s!", params);
-                long userId = Long.parseLong(params.split("/")[1]);
-                List<Entry<Long, Long>> results = playerData.sortMap(playersScore);
-                scorePosition.clear();
+        server.createContext("/", new FetchUserPositionHandler());
 
-                if (!playersScore.containsKey(userId)) {
-                    scorePosition.add(new ScorePosition(userId, 0L, results.size()+1));
-                    playersScore.put(userId, 0L);
-                } else {
-                    results.forEach(res -> {
-                        if(res.getKey() == userId){
-                            scorePosition.add(new ScorePosition(userId, res.getValue(), results.indexOf(res)+1));
-                        }
-                    });
-                }
-                System.out.println(scorePosition);
+        server.createContext("/highscorelist", new HighScoreHandler());
 
-                response = objectMapper.writeValueAsBytes(scorePosition);
-                exchange.sendResponseHeaders(200, response.length);
-                OutputStream os = exchange.getResponseBody();
-                os.write(response);
-                os.close();
-            } else {
-                exchange.sendResponseHeaders(405, -1); // 405 Method Not Allowed
-            }
-            exchange.close();
-        }));
+        server.createContext("/score", new ScoreRegisterHandler());
 
-        //GET HighScore List
-        server.createContext("/highscorelist", (exchange -> {
-            byte[] response;
-            if ("GET".equals(exchange.getRequestMethod())) {
-                List<Entry<Long, Long>> results = playerData.sortMap(playersScore);
-
-                scorePosition.clear();
-
-                results.stream().limit(20).forEach(res -> {
-                    scorePosition.add(new ScorePosition(res.getKey(), res.getValue(), results.indexOf(res)+1));
-                });
-
-                response = objectMapper.writeValueAsBytes(scorePosition);
-                exchange.sendResponseHeaders(200, response.length);
-                OutputStream os = exchange.getResponseBody();
-                os.write(response);
-                os.close();
-
-            } else {
-                exchange.sendResponseHeaders(405, -1); // 405 Method Not Allowed
-            }
-
-            exchange.close();
-        }));
-
-        //POST
-        server.createContext("/post", (exchange -> {
-            if ("POST".equals(exchange.getRequestMethod())) {
-                PlayerData player = objectMapper.readValue(exchange.getRequestBody(), PlayerData.class);
-
-                if(playersScore.containsKey(player.getUserId())){
-                    playersScore.replace(player.getUserId(), player.getPoints() + playersScore.get(player.getUserId()));
-                } else {
-                    playersScore.put(player.getUserId(), player.getPoints());
-                }
-                exchange.sendResponseHeaders(200, 0);
-            } else {
-                exchange.sendResponseHeaders(405, -1); // 405 Method Not Allowed
-            }
-
-            exchange.close();
-        }));
-
-        server.setExecutor(null); // creates a default executor
+        server.setExecutor(null);
         server.start();
     }
 }
